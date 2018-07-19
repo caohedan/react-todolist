e14711a6e54b52033293d86617510011ab0db9d4
caohedan <m18390990364@163.com>
2018-07-19 09:39:15 +0800

    'reset'

--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import TodoList from './Components/TodoList';
+
 export default class Todo extends Component {
   constructor(props) {
     super();
@@ -16,8 +16,13 @@ export default class Todo extends Component {
     this.setState({ todos: list });
     event.target.previousSibling.value = '';
   };
-  checkItem = e => {
-    this.setState({ todos: e });
+  checkItem = id => {
+    let item = this.state.todos.find(x => x.id == id);
+    if (item !== undefined) {
+      item.complete = !item.complete;
+    }
+
+    this.setState({ todos: this.state.todos });
   };
   setStatusofListByStatus = status => {
     this.setState({ statusOfList: status });
@@ -40,7 +45,7 @@ export default class Todo extends Component {
         return element.complete;
       }
     };
-    const result = todos.filter(filterExecuter['all']);
+    const result = todos.filter(filterExecuter[status]);
     return result;
   };
   handOnKeyPress = (event, itemId) => {
@@ -53,6 +58,26 @@ export default class Todo extends Component {
   };
 
   render() {
+    let list = [];
+    // alert(this.state.todos[0])
+    list = this.filterByStatus(this.state.todos, this.state.statusOfList).map(
+      element => (
+        <li
+          onDoubleClick={e => this.edit(e, element.id)}
+          onKeyPress={(e, itemId) => this.handOnKeyPress(e, element.id)}
+          className={element.complete ? 'checked' : ''}
+          key={element.id}
+        >
+          <input
+            name="done-todo"
+            type="checkbox"
+            className="done-todo"
+            onChange={this.checkItem.bind(this, element.id)}
+          />
+          {element.name}
+        </li>
+      )
+    );
     return (
       <div>
         <div>
@@ -62,13 +87,7 @@ export default class Todo extends Component {
           </div>
         </div>
         <br />
-        <TodoList
-          todoList={this.filterByStatus(
-            this.state.todos,
-            this.state.statusOfList
-          )}
-          checkItem={e => this.checkItem(e)}
-        />
+        <ol>{list}</ol>
         <ul id="filters">
           <li>
             <a
